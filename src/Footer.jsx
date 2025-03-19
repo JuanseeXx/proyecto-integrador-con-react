fc203e85e1ca0b582b43b7d0b9b47764963d086e
JuanseeXx <diaztapiassebastian@gmail.com>
2025-03-18 22:48:02 -0500

    feat empezamos el proyecto integrador haciendo el footer

---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+    return(
+        <footer>
+            <div className="contenedor-footer">
+                <div className="content-foo">
+                    <h4 className="title-foo">Teléfono</h4>
+                    <p className="cw">+57 315 987 6215</p>
+                </div>
+                <div className="content-foo">
+                    <h4 className="title-foo">Email</h4>
+                    <p className="cw">contactlattelugar@gmail.com</p>
+                </div>
+                <div className="content-foo">
+                    <h4 className="title-foo">Location</h4>
+                    <p className="cw">Cra. 43A #5 A 113 LOCAL 121, El Poblado, Medellín, Antioquia</p>
+                </div>
+            </div>
+            <h2 className="titulo-final">© Juan Sebastián Díaz Tapias | Katerin Gutierrez Castañeda</h2>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Footer from './Footer'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Footer />
+  </StrictMode>,
+)
